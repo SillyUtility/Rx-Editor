6bde5f3fcd74d40ed29f37e279581f09e5268c43
Eddie Hillenbrand <eddie@graphdyne.com>
2021-01-17 08:05:03 -0800

    Clean up and refactor Application runtime

--- Rx Editor/Application.m
+++ Rx Editor/Application.m
@@ -10,6 +10,8 @@
 #import <Carbon/Carbon.h>
 #import <SillyLog/SillyLog.h>
 
+#import "RXESDefParser.h"
+
 #import "Application.h"
 
 @implementation Application {
@@ -27,21 +29,40 @@
     if (!(self = [super init]))
         return self;
 
-    _app = [SBApplication applicationWithBundleIdentifier:ident];
-    SLYTrace(@"_app %@ %@", ident, _app);
-
     _bundle = [NSBundle bundleWithIdentifier:ident];
-    SLYTrace(@"bundle %@ %@", ident, _bundle);
-    SLYTrace(@"infoDict %@", _bundle.infoDictionary);
-
+    if (!_bundle) {
+        // TODO: indicate app not found
+        return nil;
+    }
     _name = _bundle.infoDictionary[(__bridge NSString *)kCFBundleNameKey];
 
-    err = OSACopyScriptingDefinitionFromURL((__bridge CFURLRef)_bundle.bundleURL, 0, &scriptingDefinitionData);
+    _app = [SBApplication applicationWithBundleIdentifier:ident];
+    if (!_app) {
+        // TODO: indicate scriptable app not found
+        return nil;
+    }
 
-    NSLog(@"data %@", (__bridge id)scriptingDefinitionData);
-
+    err = OSACopyScriptingDefinitionFromURL(
+        (__bridge CFURLRef)_bundle.bundleURL,
+        0,
+        &scriptingDefinitionData
+    );
+    if (err != noErr) {
+        // TODO: report unable to copy scripting definition
+        return nil;
+    }
+    [self parseScriptingDefinitionData:scriptingDefinitionData];
 
     return self;
+}
+
+- (void)parseScriptingDefinitionData:(CFDataRef)scdata
+{
+    NSData *data;
+    RXESDefParser *parser;
+
+    data = (__bridge NSData *)scdata;
+    parser = [[RXESDefParser alloc] initWithData:data];
 }
 
 @end
